809d34c0727d8c92cc08a9ea344966399e34729b
zhiyan <u1207314@utah.edu>
2021-11-14 22:45:51 -0700

    define neural network class architecture

---
+++ neural_network.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class NeuralNetworkClassifier:
+    def __init__(self):
+        pass
+    
+    def __str__(self):
+        pass
+
+    def forward_propagation(self):
+        pass
+
+    def backward_propagation(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def predict_prob(self):
+        pass
+
+    def get_params(self):
+        pass
