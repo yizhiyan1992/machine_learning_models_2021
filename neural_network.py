05437b1c3a65a6748b7b34235526dcb4fc6135fb
zhiyan <u1207314@utah.edu>
2021-11-14 23:07:46 -0700

    add initial hyper-params for neural network

--- neural_network.py
+++ neural_network.py
@@ -2,9 +2,22 @@ import numpy as np
 
 
 class NeuralNetworkClassifier:
-    def __init__(self):
-        pass
-    
+    def __init__(
+        self,
+        *,
+        max_iter: int = 1000,
+        learning_rate: float = 0.01,
+        num_layer: int = 3,
+        num_neurons: int = 20,
+        l2_regularization: float = 0
+    ):
+
+        self.max_iter = max_iter
+        self.learning_rate = learning_rate
+        self.num_layer = num_layer
+        self.num_neurons = num_neurons
+        self.l2_regularization = l2_regularization
+
     def __str__(self):
         pass
 
